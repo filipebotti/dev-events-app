7aef0c4f8fbe5d8e07ee4855613512f3a17b02fc
Filipe Botti <filipebotti@hotmail.com>
2017-04-06 16:01:39 -0300

    adjust app js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Hello!</Text>
+            </View>
+        )
+    }
+}
