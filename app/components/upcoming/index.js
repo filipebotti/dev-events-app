156aeea6142d9dc9cbf67f583c7f1964f74564b4
Filipe Botti <filipebotti@hotmail.com>
2017-04-07 19:17:02 -0300

    added listview Close #4

--- app/components/upcoming/index.js
+++ app/components/upcoming/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import { View, StyleSheet, Text} from 'react-native'
+import { View, StyleSheet, Text, ListView } from 'react-native'
+import Header from '../shared/header'
+
+const mock = [
+    { title: '6º Meetup NugCE', location: 'Fortaleza', date: 'Apr, 10'},
+    { title: 'Flisol', location: 'Fortaleza', date: 'Apr, 10'}
+]
+
 
 class UpcomingPage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context)
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+
+        this.state = {
+            dataSource: dataSource.cloneWithRows(mock)
+        }
+
+        this.renderEventsRow = this.renderEventsRow.bind(this)
+    }
+
+    renderEventsRow(item) {
+        return <Text>{item.title}</Text>
+    }
+
     render() {
-        return(        
-            <View style={{flex: 1}}>
-                <Text>Upcoming events</Text>
-            </View>
+        return(    
+            <View style={{flex:1}}>
+                <Header title={'Upcoming Events'}/>
+                <View style={{flex:1}}>
+                    <ListView                         
+                        renderRow={this.renderEventsRow}
+                        dataSource={this.state.dataSource}
+                    />
+                </View>
+            </View>    
         )
     }
 }
