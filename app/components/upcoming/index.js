3f6583af3f5d3a133315359e60b63ff2fb3fed7f
Filipe Botti <filipebotti@hotmail.com>
2017-04-07 12:01:02 -0300

    Started upcoming See #1

---
+++ app/components/upcoming/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, StyleSheet, Text} from 'react-native'
+
+class UpcomingPage extends React.Component {
+    render() {
+        <View>
+            <Text>Upcoming events</Text>
+        </View>
+    }
+}
