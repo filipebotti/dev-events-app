16d5e36fdf3094c62e76f2d0e179198ad2983405
Filipe Botti <filipebotti@hotmail.com>
2017-04-07 12:26:09 -0300

    hide statusbar

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, StatusBar } from 'react-native'
 import UpcomingPage from './components/upcoming/index'
 
 export default class App extends React.Component {
@@ -7,6 +7,7 @@ export default class App extends React.Component {
     render() {
         return (
             <View style={{flex:1}}>
+                <StatusBar hidden={true}/>
                 <UpcomingPage/>
             </View>
         )
