d384e781f6d9694ae60c8931fba128b521e49437
Filipe Botti <filipebotti@hotmail.com>
2017-04-07 21:24:17 -0300

    add some style to event item

--- app/components/shared/event-list-item/index.js
+++ app/components/shared/event-list-item/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-// const
+const styles = {
+    container: {
+        height: 70,
+        backgroundColor: '#ecf0f1',
+        borderBottomColor: '#bdc3c7',
+        borderBottomWidth: 0.5,        
+        justifyContent: 'center',
+        padding: 5
+    },
+    title: {
+        fontSize: 16
+    },
+    subTitle: {
+        fontSize:12,
+        color: '#95a5a6'
+    }
+}
 
 
 const EventListItem = ({ event }) => {
     return (
-        <View>
-            <Text>{event.title}</Text>
-            <Text>{event.location}</Text>
-            <Text>{event.date}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>{event.title}</Text>
+            <Text style={styles.subTitle}>{event.location}</Text>
+            <Text style={styles.subTitle}>{event.date}</Text>
         </View>
     )
 }
