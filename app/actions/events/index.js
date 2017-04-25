ace3ddc31ad2ffb96ba35ba345a9a219838e71eb
Filipe Botti <filipebotti@hotmail.com>
2017-04-25 00:02:42 -0300

    remove unused actions

--- app/actions/events/index.js
+++ app/actions/events/index.js
@@ -1,28 +1,9 @@
 import ActionTypes from '../../actions'
 import LocalStorage from '../../services/localStorage'
 
-async function requestEvents() {
-    try {
-        
-        let events = LocalStorage.getCards();
-        
-        dispatch({
-            type: ActionTypes.FETCH_EVENTS_SUCCESS,
-            events: events
-        })
-
-    } catch(e) {
-        dispatch({
-            type: ActionTypes.FETCH_EVENTS_FAIL,
-            error: e
-        })
+export function fetchEvents() {    
+    return { 
+        type: ActionTypes.FETCH_EVENTS_REQUEST
     }
-
-}
-
-export function fetchEvents() {
-
-    dispatch({ type: ActionTypes.FETCH_EVENTS_REQUEST })
-
-    requestEvents()
+    
 }
