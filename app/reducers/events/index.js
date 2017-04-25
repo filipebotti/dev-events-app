122e3354d9d23d9b28d0d2604bc7a66c899c793f
Filipe Botti <filipebotti@hotmail.com>
2017-04-24 22:38:46 -0300

    events reducer

---
+++ app/reducers/events/index.js
@@ -0,0 +1,14 @@
+import initialState from '../initialState'
+import ActionTypes from '../../actions'
+
+export default function(state = initialState, action) {
+    
+    switch(action.type) {
+        case ActionTypes.FETCH_EVENTS_REQUEST:            
+            return Object.assign({}, state, { isFetching: true})
+        case ActionTypes.FETCH_EVENTS_SUCCESS:
+            return Object.assign({}, state, { isFetching: false, events: action.events})
+        case ActionTypes.FETCH_EVENTS_FAIL:
+            return Object.assign({}, state, { isFetching: false, error: action.error})
+    }
+}
