57a8a6a0551638675a612ff2df62d5330b94e2ed
Filipe Botti <filipebotti@hotmail.com>
2017-04-25 00:02:59 -0300

    apply saga middleware

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+//reducers
 import events from './app/reducers/events'
 import notification from './app/reducers/notification'
+//saga watchers
+import eventsSaga from './app/watchers/events'
 import App from './app/app'
 
-let store = createStore(combineReducers({ events, notification }))
 
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(combineReducers({ events, notification }), applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(eventsSaga)
 
 export default class DevEventsMobile extends Component {
   render() {
