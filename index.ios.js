a408f94a80803575fbb1536469791d1f28d5dc1a
Filipe Botti <filipebotti@hotmail.com>
2017-04-25 22:36:47 -0300

    apply redux logger middleware

--- index.ios.js
+++ index.ios.js
@@ -3,16 +3,18 @@ import { AppRegistry } from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
+import reduxLogger from 'redux-logger'
 //reducers
 import events from './app/reducers/events'
-import notification from './app/reducers/notification'
 //saga watchers
 import eventsSaga from './app/watchers/events'
 import App from './app/app'
 
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(combineReducers({ events, notification }), applyMiddleware(sagaMiddleware))
+const store = createStore(combineReducers({ events }), 
+                          applyMiddleware(sagaMiddleware), 
+                          applyMiddleware(reduxLogger))
 
 sagaMiddleware.run(eventsSaga)
 
