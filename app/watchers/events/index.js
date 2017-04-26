ac4d39e849252abfcbd17a40ca9bbb6e97091933
Filipe Botti <filipebotti@hotmail.com>
2017-04-25 22:37:16 -0300

    add notification methods to event watcher

--- app/watchers/events/index.js
+++ app/watchers/events/index.js
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
 import LocalStorage from '../../services/localStorage'
 import ActionTypes from '../../actions'
+import NotificationService from '../../services/notification'
 
 function* fetchCards(action) {    
     try {
@@ -11,8 +12,29 @@ function* fetchCards(action) {
     }
 }
 
+function* checkPermission(action) {    
+    
+    const permissions = yield call (NotificationService.getPermissions)
+
+    if(!permissions.alert && !permissions.alert && !permissions.badge) {
+
+        const requested = yield call(NotificationService.requestPermissions)
+        if(requested) {
+            NotificationService.scheduleLocalnotification(action.event)
+            yield put({ type: ActionTypes.NOTIFICATION_REGISTER_SUCCESS, event: action.event })   
+        }
+    } else {
+
+        NotificationService.scheduleLocalnotification(action.event)
+        yield put({ type: ActionTypes.NOTIFICATION_REGISTER_SUCCESS, event: action.event })          
+    }            
+}
+
+
+
 function* watcher() {
     yield takeLatest(ActionTypes.FETCH_EVENTS_REQUEST, fetchCards)
+    yield takeLatest(ActionTypes.NOTIFICATION_CHECK_PERMISSIONS_REQUESTED, checkPermission)
 }
 
 export default watcher;
