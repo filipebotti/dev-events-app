be5511cacc2ea73c864bda888b9d522bd8130db8
Filipe Botti <filipebotti@hotmail.com>
2017-04-25 22:37:47 -0300

    move notification reducer to event reducer

--- app/reducers/events/index.js
+++ app/reducers/events/index.js
@@ -1,8 +1,22 @@
 import initialState from '../initialState'
 import ActionTypes from '../../actions'
 
-export default function(state = initialState, action) {
+function registerEvent(events, eventId) {   
     
+    
+    let event = events.find(item => item.id == eventId)    
+    event.registered = true
+    return [...events]
+}
+
+function unregisterEvent(events, eventId) {
+
+    let event = events.find(item => item.id == eventId)
+    event.registered = false
+    return [...events]
+}
+
+export default function(state, action) {
     switch(action.type) {
         case ActionTypes.FETCH_EVENTS_REQUEST:            
             return Object.assign({}, state, { isFetching: true})
@@ -10,7 +24,11 @@ export default function(state = initialState, action) {
             return Object.assign({}, state, { isFetching: false, events: action.events})
         case ActionTypes.FETCH_EVENTS_FAIL:
             return Object.assign({}, state, { isFetching: false, error: action.error})
+        case ActionTypes.NOTIFICATION_REGISTER_SUCCESS: 
+            return Object.assign({}, state, { events: registerEvent(state.events, action.event.id)})
+        case ActionTypes.NOTIFICATION_CANCEL_SUCCESS:
+            return Object.assign({}, state, { events: unregisterEvent(state.events, action.event.id)})
         default:
-            return state
+            return state || initialState            
     }
 }
